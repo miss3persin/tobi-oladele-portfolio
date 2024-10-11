58019aa7085c427b2f543773c54b6d9d54736dc9
Segun <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2024-10-11 06:38:33 +0100

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='py-8'>
+    <div className='container'>
+      <h1 className='text-3xl font-bold text-center'>My NextJS Boilerplate</h1>
+    </div>
+  </section>
+  )
+}
+
+export default page
